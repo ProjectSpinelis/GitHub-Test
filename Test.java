592ef87db5621cf7374b18ffd1ff68ab81e9c27f
Andreas042105 <t8230224@aueb.gr>
2024-11-10 22:37:40 +0200

    Update Test.java

--- Test.java
+++ Test.java
@@ -2,6 +2,7 @@ public class Test {
     public static void main (String[] args) {
         System.out.println("8230057");
 	System.out.println("8230098");
+	System.out.println("8230224");
     }
 }
 
