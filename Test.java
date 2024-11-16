a900241c5d4502374e1655f29385d201dc4aa463
Odysseas Kerasaridis <okerasar@gmail.com>
2024-11-16 17:25:29 +0200

    Update Test.java

--- Test.java
+++ Test.java
@@ -7,10 +7,6 @@ public class Test {
 	System.out.println("8230163");
 	System.out.println("8230066");
 	System.out.println("8230159");
-<<<<<<< HEAD
-	System.out.println("8230002");    
-=======
-	
->>>>>>> a65ba461cf5b7458b359fca095cd2fccafb948c3
+	System.out.println("8230002"); 
     }
 }
