e98bf3bfc031cd761333d7e14edd3281c1002139
agfereniki <t8230002@aueb.gr>
2024-11-16 17:02:21 +0200

    update fereniki

--- Test.java
+++ Test.java
@@ -6,6 +6,7 @@ public class Test {
 	System.out.println("8230143");
 	System.out.println("8230163");
 	System.out.println("8230066");
-	System.out.println("8230159");    
+	System.out.println("8230159");
+	System.out.println("8230002");    
     }
 }
